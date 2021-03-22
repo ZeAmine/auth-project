0e0dff4da670b3c405177d9b183676eb96d00df8
Amine ZEGMOU <amine95170@gmail.com>
2021-03-22 21:36:53 +0100

    Create active/innactive button

--- profile.php
+++ profile.php
@@ -4,11 +4,18 @@
         <div class="container">
             <h1>Votre Profile</h1>
             <div class="section-left">
-                <form action="includes/shorturl.inc.php" method="POST" class="form-url">
-                    <input type="url" name="input_url" placeholder="Entrez votre URL ici" pattern="https://.*"
-                           size="30">
-                    <button type="submit" name="submit_url" value="shorten">Raccourcir</button>
-                </form>
+                <div class="container-url">
+                    <h4>Vos liens raccourci :</h4>
+                    <ul class="list-url">
+                        <?php if (isset($_SESSION["short"]) && isset($_SESSION["url"])): ?>
+                            <li class="item-url">
+                                <a href="<?= $_SESSION["url"] ?>"
+                                   target="_blank">http://reducelink/v.php?key=<?= $_SESSION["short"] ?></a>
+                                <button class="state" onclick="changeState()">active</button>
+                            </li>
+                        <?php endif; ?>
+                    </ul>
+                </div>
             </div>
             <div class="section-right">
                 <div class="block-user">
@@ -23,5 +30,24 @@
             </div>
         </div>
     </section>
+
+    <script type="text/javascript">
+        function changeState() {
+            const btn = document.querySelector('.state')
+            const result = btn.classList.toggle("innative")
+
+            if (result) {
+                btn.innerText = "innactive"
+                btn.style.color = "darkred"
+                btn.style.border = "1px solid #912323"
+                btn.style.background = "#ee9393"
+            } else {
+                btn.innerText = "active"
+                btn.style.color = ""
+                btn.style.border = ""
+                btn.style.background = ""
+            }
+        }
+    </script>
 
 <?php require_once('footer.php'); ?>
