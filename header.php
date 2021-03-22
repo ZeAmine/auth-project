87ee8275c0b40e6f4dfc495640d2a643617b8b5e
Amine ZEGMOU <amine95170@gmail.com>
2021-03-22 12:58:00 +0100

    Update Profil style

--- header.php
+++ header.php
@@ -14,22 +14,13 @@
     <nav>
         <a href="index.php" class="nav-brand">ReduceLink.</a>
         <ul class="nav-items">
-            <?php
-            if (isset($_SESSION["user"])) {
-                echo "<li><a href='profile.php'>Profile</a></li>";
-                echo "<li><a href='./includes/logout.inc.php'>Se deconnecter</a></li>";
-            } else {
-                echo "<li><a href='signin.php'>Se connecter</a></li>";
-                echo "<li><a href='signup.php'>S'inscrire</a></li>";
-            }
-            ?>
-            <!--            --><?php //if (isset($_SESSION["user"])): ?>
-            <!--                <li><a href="profil.php">Profil</a></li>-->
-            <!--                <li><a href="logout.php">Se deconnecter</a></li>-->
-            <!--            --><?php //else: ?>
-            <!--                <li><a href="signin.php">Se connecter</a></li>-->
-            <!--                <li><a href="signup.php">S'inscrire</a></li>-->
-            <!--            --><?php //endif; ?>
+            <?php if (isset($_SESSION["user"])): ?>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="./includes/logout.inc.php">Se deconnecter</a></li>
+            <?php else: ?>
+                <li><a href="signin.php">Se connecter</a></li>
+                <li><a href="signup.php">S'inscrire</a></li>
+            <?php endif; ?>
         </ul>
     </nav>
 </header>
