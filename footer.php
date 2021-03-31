ecbe197003300fa999a498de4d64e4ad4a411da7
BonneauVincent <vincent.bonneau@hetic.net>
2021-03-31 18:02:44 +0200

    css 6 fini
    c'est fini

--- footer.php
+++ footer.php
@@ -4,12 +4,6 @@
         <!-- <a href="index.php" class="footer-logo"></a> -->
         <div class="legal">
             <div class="left">Copyright ©2020 Projet PHP | Patxi Manzano | Amine Zegmou | Alexis Kessab | Vincent Bonneau | </div>
-            <ul>
-                <li><span>Notice Légale</span></li>
-                <li><span>Politique de Confidentialité</span></li>
-                <li><span>Cookies</span></li>
-                <li><span>Contactez le FC MODIST</span></li>
-            </ul> 
         </div>
         <p>Donnés venant de PHPMyadmin</p>
     </div>
