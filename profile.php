931f2f0e3b4a38f1aed963a4b42db65020e1e276
Amine ZEGMOU <amine95170@gmail.com>
2021-03-31 16:25:09 +0200

    Update profil file

--- profile.php
+++ profile.php
@@ -14,8 +14,8 @@ $pdo = new PDO("mysql:host=localhost;dbname=auth-project", $username, $password,
         <div class="top-profil">
             <div class="left-profil">
                 <h1>Bienvenue </h1>
-                <p>Name : <?= $_SESSION["user"] ?></p>
-                <p>Mail : <?= $_SESSION["email"] ?></p>
+                <p>Nom : <?= $_SESSION["user"] ?></p>
+                <p>E-mail : <?= $_SESSION["email"] ?></p>
             </div>
             <div class="right-profil">
                 <img src="img/profilpic.jpg">
@@ -23,17 +23,16 @@ $pdo = new PDO("mysql:host=localhost;dbname=auth-project", $username, $password,
         </div>
         <div class="mid-profil">
             <div class="form-profil">
-                <form action="includes/shorturl.inc.php" method="POST" class="form-url">
-                    <input type="url" name="input_url" placeholder="Coller votre URL ici pour le raccourcir"
+                <form action="includes/urluser.inc.php" method="POST" class="form-url">
+                    <input type="url" name="url_user" placeholder="Coller votre URL ici pour le raccourcir"
                            pattern="https://.*" size="30"
                            required>
-                    <button class="btnCheck" type="submit" name="submit_url" value="shorten">Raccourcir</button>
+                    <button class="btnCheck" type="submit" name="submit_url_user" value="shorten">Raccourcir</button>
                 </form>
             </div>
             <div class="display-profil">
                 <?= displayUrl($pdo, $user) ?>
             </div>
-
         </div>
         <div class="bottom-profil">
             <img src="img/bottompic.jpg">
@@ -41,7 +40,6 @@ $pdo = new PDO("mysql:host=localhost;dbname=auth-project", $username, $password,
                 <h1>Voici quelques statistiques : </h1>
                 <p class="p-body">nombre d'URL transformé : <?= $_SESSION["nbUrl"] ?> </p>
             </div>
-
         </div>
     </div>
 </section>
